f409ccf16d0f85fbb0f4929f93d636d2d62fbe58
Kir Neboula <newbie13xd@outlook.com>
2018-07-14 12:53:31 +0200

    idk

--- 2DCombatSim/Level.cpp
+++ 2DCombatSim/Level.cpp
@@ -1,10 +1,23 @@
 #include "Level.h"
-
-
+#include <iostream>
+#include <string>
+#include <fstream>
 
 Level::Level(int levelN) {
-	std::ifstream test("levels/level1.txt", std::ifstream::in);
+	std::ifstream *levelFile = new std::ifstream("level.txt");
 
+	if (!(levelFile->is_open())) {
+		std::cout << *perror;
+		return;
+	}
+
+	std::string input;
+
+	_level->push_back("hi");
+
+	for (int i = 0; std::getline(*levelFile, input); i++) {
+		_level->push_back(input + '\0');
+	}
 }
 
 
@@ -12,6 +25,22 @@ Level::~Level() {
 	delete[] _level;
 }
 
-char Level::getTile(char x, char y) {
+char Level::getTile(unsigned int x, unsigned int y) {
 	return 0;
+}
+
+void Level::setTile(unsigned int x, unsigned int y) {
+}
+
+void Level::printLevel() {
+	int levelHeight = _level->size();
+	std::vector<std::string>& levelRef = *_level;
+
+	for (int x = 0; x < levelHeight; x++) {
+		for (unsigned int y = 0; y < levelRef[x].size(); y++) {
+			//std::cout << x << '\t' << y;
+			printf("%c%c", levelRef[x][y], ' ');
+		}
+		printf("\n");
+	}
 }
